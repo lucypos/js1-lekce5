5337a1176c0df5b68365a184c78e0305532ac86e
Lucie Pospisilova <pospisilova.lucy@gmail.com>
2023-02-16 20:12:28 +0100

    hodina

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-import './style.css';
 
-console.log('funguju!');
+let jmeno = prompt('Zadejte své jméno')
+let prijmeni = prompt('Zadejte své příjmení')
+let vek = Number(prompt('Zadejte svůj věk'))
+
+console.log('Jméno a příjmení: ' + jmeno + ' ' + prijmeni)
+console.log('Věk: ' + vek)
+
+
+if (vek >= 65) {
+    let heslo = prompt('V pořádku. Zadejte heslo')
+    console.log('Heslo: ' + heslo)
+
+    if (heslo.length <= 8) {
+        document.body.innerHTML = 'Slabé heslo'
+    } else {
+        document.body.innerHTML = 'Heslo je v pořádku.'
+    }
+
+} else {
+    (vek < 65) 
+    document.body.innerHTML = 'Nízký věk'
+    }
+ 
